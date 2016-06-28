5c3d2865fd8d1e1d6ef22a8bf61dff378df19145
Hao Du <duhao1993@gmail.com>
2016-06-28 13:44:40 +0800

    create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : hello_world.cpp
+// Author      : Hao Du
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello world" << endl; // prints !!!Hello World!!!
+	return 0;
+}
